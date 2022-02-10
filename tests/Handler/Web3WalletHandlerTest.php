719fe193ee9c67024fb24cc0f3eb4911525e4a7e
Pierre Declémy <pierredeclemy@gmail.com>
2022-02-10 12:54:25 +0100

    Adds tests

--- tests/Handler/Web3WalletHandlerTest.php
+++ tests/Handler/Web3WalletHandlerTest.php
@@ -21,6 +21,23 @@ class Web3WalletHandlerTest extends TestCase
     "message": "localhost:8080 wants you to sign in with your Ethereum account:\n0x42d16fbE856CA5fDCD3C9cFE1672d9183fa01534\n\nSign in with Ethereum to the app.\n\nURI: http://localhost:8080\nVersion: 1\nChain ID: 1\nNonce: N4EkiSkl\nIssued At: 2022-02-10T11:07:24.835Z",
     "signature": "'.self::SIG.'"
 }';
+    const JSON_MESSAGE = '{
+    "message": {
+        "domain": "www.example.com",
+        "uri": "http://127.0.0.1:8000/web3_login",
+        "version": 1,
+        "chain-id": 1,
+        "nonce": "VlMdgoW1",
+        "address": "0xedaab2a15961a7b6581f4f8c60b32f9bd8802f8c",
+        "statement": "Sign in with Ethereum to the app.",
+        "request-id": "foo",
+        "resources": [
+            "http://127.0.0.1",
+            "http://127.0.0.2"
+        ]
+    },
+    "signature": "0x11afc648826543977217e3cb0eab36e4f8197b908ef3396890d6712961b4d74a2809c624b43dca1447eace1b258fae95bd95f53f380ae9b327162390d2d5a4601b"
+}';
 
     public function testGenerateNonce()
     {
@@ -49,7 +66,7 @@ class Web3WalletHandlerTest extends TestCase
         $this->assertTrue($this->createHandler()->checkSignature($handler->prepareMessage($message), $data['signature'], $message->getAddress()));
     }
 
-    public function testCreateMessage()
+    public function testCreateMessageFromString()
     {
         $handler = $this->createHandler();
         $data = \json_decode(self::EIP4361_MESSAGE, true);
@@ -65,13 +82,25 @@ class Web3WalletHandlerTest extends TestCase
         $this->assertIsInt($handler->getRecidFromSignature(self::SIG));
     }
 
+    public function testCreateMessageFromArray()
+    {
+        $handler = $this->createHandler();
+        $data = \json_decode(self::JSON_MESSAGE, true);
+        $message = $handler->createMessageFromArray($data['message']);
+        $this->assertInstanceOf(Message::class, $message);
+    }
+
     public function testPrepareMessage()
     {
         $handler = $this->createHandler();
         $data = \json_decode(self::EIP4361_MESSAGE, true);
         $message = $handler->createMessageFromString($data['message']);
         $plainTextMessage = $handler->prepareMessage($message);
+        $this->assertIsString($plainTextMessage);
 
+        $data = \json_decode(self::JSON_MESSAGE, true);
+        $message = $handler->createMessageFromArray($data['message']);
+        $plainTextMessage = $handler->prepareMessage($message);
         $this->assertIsString($plainTextMessage);
     }
 
